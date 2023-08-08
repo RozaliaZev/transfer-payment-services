6713b472225238f07212916346b896a6ca73d700
Zevyakina Rozalia <rozalka.zevyakina@yandex.ru>
2023-08-08 13:40:32 +0300

    actual schema

--- internal/payment_service/service/schema.sql
+++ internal/payment_service/service/schema.sql
@@ -1,15 +1,15 @@
 -- Создание таблицы с балансами пользователей
 CREATE TABLE IF NOT EXISTS balances (
-    sender_id SERIAL PRIMARY KEY,
-    balance FLOAT(2) NOT NULL
-);
+		sender_id VARCHAR(20) PRIMARY KEY,
+		balance FLOAT(2) NOT NULL
+	);
 
 -- Создание таблицы с переводами
 CREATE TABLE IF NOT EXISTS transfers (
-    request_id UUID PRIMARY KEY,
-    sender_id INT NOT NULL,
-    amount FLOAT(2) NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20) DEFAULT 'successful',
-    FOREIGN KEY (sender_id) REFERENCES balances (sender_id) ON DELETE CASCADE
-);
+        request_id VARCHAR(20) NOT NULL,
+        sender_id VARCHAR(20) NOT NULL,
+        amount FLOAT(2) NOT NULL,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        status VARCHAR(20) DEFAULT 'successful',
+        FOREIGN KEY (sender_id) REFERENCES balances_test (sender_id) ON DELETE CASCADE
+    );
